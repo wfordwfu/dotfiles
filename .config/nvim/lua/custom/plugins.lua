16998562f5f67900814bce2fb8be7a81f26776bd
Will Ford <7543630+wfordwfu@users.noreply.github.com>
2022-12-21 13:03:50 -0500

    add colorscheme

---
+++ .config/nvim/lua/custom/plugins.lua
@@ -0,0 +1,6 @@
+return function(use)
+  use({
+    "ellisonleao/gruvbox.nvim",
+  })
+end
+
--- .config/nvim/lua/user/init.lua
+++ .config/nvim/lua/user/init.lua
@@ -1,3 +1,4 @@
 require "user.kickstart"
 require "user.options"
 require "user.keymaps"
+require "user.interface"
