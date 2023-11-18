7a56262c8d86f771193b1567d467fff9498d9231
Will Ford <7543630+wfordwfu@users.noreply.github.com>
2023-11-18 17:15:05 -0500

    back out code cli stuff

--- bootstrap.sh
+++ bootstrap.sh
@@ -56,11 +56,11 @@ if hash code 2>/dev/null; then
   echo "##################################################"
   echo "VSCode cli is available"
   echo "##################################################"
-  if ! code --list-extensions | grep -q "github.copilot"; then
+#  if ! code --list-extensions | grep -q "github.copilot"; then
     # Install GitHub Copilot
-    code --install-extension GitHub.copilot
-    code --install-extension GitHub.copilot-chat
-  fi
+#    code --install-extension GitHub.copilot
+#    code --install-extension GitHub.copilot-chat
+#  fi
 #  code --install-extension mutantdino.resourcemonitor
 #  code --install-extension yzhang.markdown-all-in-one
 #  code --install-extension vscode-icons-team.vscode-icons
