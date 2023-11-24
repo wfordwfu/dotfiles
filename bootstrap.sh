ec8838f226f94fb2d90f020d9b399ffc455c1399
Will Ford <7543630+wfordwfu@users.noreply.github.com>
2023-11-24 12:28:36 -0500

    add error handling to bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+set -o errexit
+set -o errtrace
+set -o nounset
+set -o pipefail
+
 create_symlinks() {
     # Get the directory in which this script lives.
     script_dir=$(dirname "$(readlink -f "$0")")
@@ -52,23 +57,3 @@ else
   fi
 fi
 
-if hash code 2>/dev/null; then
-  echo "##################################################"
-  echo "VSCode cli is available"
-  echo "##################################################"
-#  if ! code --list-extensions | grep -q "github.copilot"; then
-    # Install GitHub Copilot
-#    code --install-extension GitHub.copilot
-#    code --install-extension GitHub.copilot-chat
-#  fi
-#  code --install-extension mutantdino.resourcemonitor
-#  code --install-extension yzhang.markdown-all-in-one
-#  code --install-extension vscode-icons-team.vscode-icons
-#  code --install-extension oderwat.indent-rainbow
-#  code --install-extension esbenp.prettier-vscode
-#  code --install-extension vscodevim.vim
-else
-  echo "##################################################"
-  echo "VSCode cli not available"
-  echo "##################################################"
-fi
