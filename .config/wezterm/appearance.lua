a6b84579d5d85f954b89c7385b3368f1cfabf3e7
Will Ford <7543630+wfordwfu@users.noreply.github.com>
2024-12-08 17:32:52 -0500

    add to wezterm config

--- .config/wezterm/appearance.lua
+++ .config/wezterm/appearance.lua
@@ -6,8 +6,17 @@ function module.apply_to_config(config)
 
   config.color_scheme = "Dracula"
   config.font = wezterm.font "CaskaydiaCove Nerd Font"
+  config.font_size = 14.0
   config.macos_window_background_blur = 30
   config.window_background_opacity = 0.80
+  config.window_padding = {
+      left = 10,
+      right = 10,
+      top = 10,
+      bottom = 10,
+  }
+  config.initial_cols = 120
+  config.initial_rows = 35
 end
 
 return module
--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -5,4 +5,6 @@ local config = wezterm.config_builder()
 local appearance = require("appearance")
 appearance.apply_to_config(config)
 
+config.front_end = "WebGpu"
+config.webgpu_power_preference = "HighPerformance"
 return config
