a83c5bc83a2e3c288b50e9446f8f5e44d6698681
Will Ford <7543630+wfordwfu@users.noreply.github.com>
2024-12-30 11:57:26 -0500

    add winbar with index and adjust layout

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,9 +1,60 @@
 return {
     "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
       require("lualine").setup({
         options = {
+          icons_enabled =  true,
           theme = "nord",
+          component_separators = "|",
+          section_separators = "",
+          globalstatus = true,
+        },
+        sections = {
+          lualine_a = {
+            {
+              function()
+                return vim.api.nvim_win_get_number(0)
+              end,
+            }
+          },
+          lualine_b = { "branch", "diagnostics" },
+          lualine_c = { "mode" },
+        },
+        inactive_sections = {},
+        winbar = {
+          lualine_a = {
+            {
+              function()
+                return vim.api.nvim_win_get_number(0)
+              end,
+            }
+          },
+          lualine_c = {
+            {
+              "filename",
+              file_status = true,
+              newfile_status = false,
+              path = 3,
+            }
+          }
+        },
+        inactive_winbar = {
+          lualine_a = {
+            {
+              function()
+                return vim.api.nvim_win_get_number(0)
+              end,
+            }
+          },
+          lualine_c = {
+            {
+              "filename",
+              file_status = true,
+              newfile_status = false,
+              path = 1,
+            }
+          }
         }
       })
     end
